8f46952343434e83eaa2361204d46787717924f2
Simon Shields <simon@lineageos.org>
2017-08-23 19:04:51 +1000

    RMX1931: recovery: update for O compatability
    Change-Id: I1f01837e18acd6971c0aaf410db87a54ad34afbc

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -2,7 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES := bootable/recovery
+LOCAL_C_INCLUDES := bootable/recovery \
+                    bootable/recovery/updater/include
+ifneq ($(TARGET_PLATFORM_DEVICE_BASE),)
+LOCAL_CFLAGS += -DUSES_BOOTDEVICE_PATH
+endif
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_qcom
 LOCAL_MODULE_TAGS := eng
