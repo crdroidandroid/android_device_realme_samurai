edf11853be8d89a20e96f8acef1331d802182015
Demon Singur <demonsingur@gmail.com>
2018-06-23 05:22:18 +0000

    RMX1931: Build separate system and vendor HIDL libs
    Change-Id: If8e2211e643bdb0ff67fb248e7fe33b78077a06e

--- device.mk
+++ device.mk
@@ -87,7 +87,9 @@ PRODUCT_COPY_FILES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0_system
 
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
