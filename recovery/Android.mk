b0a0c47c0bbdbfccb57285aa4e65d9951d607e29
YumeMichi <do4suki@gmail.com>
2018-08-10 04:15:00 +0200

    RMX1931: update recovery_updater for P
    Change-Id: I28d1133fcee0ea8010e568fda8ba6bdebb64e338

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -3,10 +3,13 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES := bootable/recovery \
+                    bootable/recovery/edify/include \
+                    bootable/recovery/otautil/include \
                     bootable/recovery/updater/include
 ifneq ($(TARGET_PLATFORM_DEVICE_BASE),)
 LOCAL_CFLAGS += -DUSES_BOOTDEVICE_PATH
 endif
+
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_qcom
 LOCAL_MODULE_TAGS := eng
