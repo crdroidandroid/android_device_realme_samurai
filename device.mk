fd160b0564ec8f748372fc9e68ac4c4d32807f4a
Bruno Martins <bgcngm@gmail.com>
2019-03-14 23:32:40 +0000

    RMX1931: Add whitelist to support exposing vendor libs
    Change-Id: I57e97048e9d1bbf7e884770de53643fb186b6204
    Signed-off-by: karthick111 <karthickrko61@gmail.com>

--- device.mk
+++ device.mk
@@ -245,6 +245,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     lineage.livedisplay@2.0-service-sdm
 
+# Native libraries whitelist
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
