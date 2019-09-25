d0391e2f094468221d69d1c4f287884e12b57e46
Bruno Martins <bgcngm@gmail.com>
2019-09-25 23:00:35 +0100

    RMX1931: Kill framework compatibility matrix with fire
    This thing is not mandatory at all and only demands extra work by having
    to keep it up-to-date whenever new interfaces are added to the device manifest
    or simply uprev'ed.
    
    Change-Id: I7c5485f201efde86ff1b277951e7faebf8b80437

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,7 +94,6 @@ TARGET_ENABLE_MEDIADRM_64 := true
 TARGET_SURFACEFLINGER_FOD_LIB := //$(DEVICE_PATH):libfod_extension.RMX1931
 
 # Hidl
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 
 # Metadata
