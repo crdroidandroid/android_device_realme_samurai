9e8d42d0845b4fd37090b0a08298e884178abf87
Jack Pham <jackp@codeaurora.org>
2020-02-09 00:32:01 -0800

    RMX1931: init.qcom.usb.rc: Prepare for removal
    Guard LOCAL_MODULE definitions for init.qcom.usb.{rc,sh}
    and init.msm.usb.configfs.rc to prepare for them getting
    moved to the USB project area and defined in Android.bp
    there. Full removal of the actual files will follow once
    they are in place at the new location.
    
    In addition, move the import of init.msm.usb.configfs.rc
    to init.qcom.usb.rc to remove a direct reference from
    init.qcom.rc. This will make it easier to consolidate
    the files later after they have moved.
    
    Change-Id: I58dfad3f6a460f9dc2f198b33ede4f210c910e07

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -48,6 +48,7 @@ LOCAL_SRC_FILES    := etc/init.recovery.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
+ifndef MODULE.TARGET.ETC.init.qcom.usb.rc
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.factory.rc
 LOCAL_MODULE_TAGS  := optional
@@ -95,6 +96,7 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := bin/init.qcom.usb.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
+endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.qcom.rc
