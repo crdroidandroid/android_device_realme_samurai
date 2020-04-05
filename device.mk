05c6599bb64ba325814dd80fe5e43c7497104542
karthick111 <karthickrko61@gmail.com>
2020-04-05 20:54:09 +0530

    RMX1931: Update NFC configuration
    Change-Id: Id561365500720e1ec79f4a1ef6730f4f43d439ef

--- device.mk
+++ device.mk
@@ -105,16 +105,9 @@ PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
 # NFC
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:system/etc/permissions/android.hardware.nfc.hcef.xml \
-    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-    $(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml \
-    $(LOCAL_PATH)/configs/nfc/nqnfcee_access.xml:system/etc/nqnfcee_access.xml \
-    $(LOCAL_PATH)/configs/nfc/nqnfcse_access.xml:system/etc/nqnfcse_access.xml
-
 PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    com.gsma.services.nfc \
     NfcNci \
     SecureElement \
     Tag
