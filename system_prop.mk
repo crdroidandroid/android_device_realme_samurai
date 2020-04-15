1b019d4a1508d722fc914884a6d3c73091cd7734
luk1337 <priv.luk@gmail.com>
2020-04-15 18:28:06 +0000

    RMX1931: Add IMS debug props
    Change-Id: I9f53f2ac66bd3b87c31fadd340b738b2aaa739c2

--- system_prop.mk
+++ system_prop.mk
@@ -81,6 +81,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # RCS and IMS
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.ims_volte_enable=1 \
+    persist.data.iwlan.enable=true \
+    persist.dbg.wfc_avail_ovr=1 \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
     persist.rcs.supported=0 \
