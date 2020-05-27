f02999db814c77c1e29d404765a2656ff58b611a
Jesse Chan <jc@lineageos.org>
2020-05-27 19:00:22 +0800

    RMX1931: override dalvik-heap configs
    Change-Id: I349bee49290e7aacbd1664d8edfa6a6f64df2652
    Signed-off-by: Jesse Chan <jc@lineageos.org>

--- product_prop.mk
+++ product_prop.mk
@@ -4,6 +4,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Dalvik
+PRODUCT_PRODUCT_PROPERTIES += \
+    dalvik.vm.heapstartsize=32m \
+    dalvik.vm.heapgrowthlimit=512m \
+    dalvik.vm.heapsize=768m \
+    dalvik.vm.heaptargetutilization=0.5 \
+    dalvik.vm.heapminfree=8m \
+    dalvik.vm.heapmaxfree=64m
+
 # Media
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.stagefright.omx_default_rank.sw-audio=16
