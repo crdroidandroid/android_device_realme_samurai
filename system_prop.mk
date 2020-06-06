d77edf852943e0f7d62f28b46b72c5072f6b1a05
karthick111 <karthickrko61@gmail.com>
2020-06-06 07:59:40 +0530

    RMX1931: Clean/Update props for Q
    Change-Id: If703057325f327a1190868066884bce2a887610c

--- system_prop.mk
+++ system_prop.mk
@@ -6,24 +6,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     bt.max.hfpclient.connections=1 \
-    persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aptxadaptive-aac-ldac \
     persist.vendor.btstack.enable.splita2dp=true \
-    persist.bluetooth.disableabsvol=true \
-    persist.bluetooth.disableinbandringing=false \
-    persist.vendor.bluetooth.modem_nv_support=true \
-    persist.vendor.btstack.iotinfo.report.enable=true \
-    persist.vendor.service.bdroid.soc.alwayson=true \
-    persist.vendor.service.bt.iot.enablelogging=true \
     qcom.bluetooth.soc=cherokee \
     vendor.bluetooth.soc=cherokee
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.light_ae.enable=1 \
-    ro.camera.hfr.enable=1 \
-    ro.camera.relight.enable=0 \
-    ro.camera.notify_nfc=1 \
-    ro.camera.temperature.limit=460 \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap
 
 # CNE
@@ -37,12 +26,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.data.profile_update=true \
     persist.vendor.data.mode=concurrent \
     ro.vendor.use_data_netmgrd=true
-
-# Display post-processing
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.display.ad=1 \
-    ro.vendor.display.ad.sdr_calib_data=/vendor/etc/OPPO_OLED_AD_calib.cfg \
-    ro.vendor.display.sensortype=2
 
 # Fingerprint
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -62,6 +45,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
     media.settings.xml=/system/etc/media_profiles_vendor.xml \
+    media.stagefright.thumbnail.prefer_hw_codecs=true \
     persist.mm.enable.prefetch=true \
     vendor.media.vpp.enable=true
 
@@ -75,18 +59,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # NFC
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.nfc.smartcard.recorder.enable=true \
     persist.sys.nfc.aid_overflow=true \
     persist.sys.nfc.antenna_area=bottom \
     persist.sys.nfc.default=on \
-    persist.sys.nfc.disPowerSave=1 \
     ro.nfc.port=I2C \
     ro.product.cuptsm=OPPO|ESE|01|02
 
 # RCS and IMS
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.ims_volte_enable=1 \
-    persist.data.iwlan.enable=true \
     persist.dbg.wfc_avail_ovr=1 \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
@@ -96,31 +77,26 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.ims.disableDebugLogs=0 \
     persist.vendor.ims.disableIMSLogs=0 \
     persist.vendor.ims.rtp.enableqxdm=3 \
-    persist.vendor.ims.vt.enableadb=3
+    persist.vendor.ims.vt.enableadb=3 \
+    persit.radio.VT_LOG_LEVEL=255 \
+    persist.vendor.ims.loglevel=255
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     DEVICE_PROVISIONED=1 \
-    gsm.lte.ca.support=1 \
-    persist.radio.custom_exp_ecc=1 \
     persist.radio.hvolte.enable=1 \
     persist.vendor.radio.atfwd.start=true \
     persist.vendor.radio.lte_vrte_ltd=1 \
     persist.vendor.radio.cs_srv_type=1 \
     persist.vendor.radio.rat_on=combine \
-    persist.vendor.radio.process_sups_ind=1 \
     ril.subscription.types=NV,RUIM \
     ro.telephony.default_network=22,22 \
     telephony.lteOnCdmaDevice=1
 
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.sensors.enable.mag_filter=true \
     ro.qti.sensors.wu=false
-
-# VoWIFI
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.data.iwlan.enable=true \
-    persist.data.iwlan.rekey=4294967295
 
 # SSR
 PRODUCT_PROPERTY_OVERRIDES += \
