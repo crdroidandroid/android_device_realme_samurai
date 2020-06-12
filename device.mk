cd48350747b2856ddf32116a60b35b63b79521fd
karthick111 <karthickrko61@gmail.com>
2020-06-12 19:31:15 +0530

    RMX1931: Add Dolby blobs from chef_sprout
    * Also import mediacodec-seccomp.policy from stock along changes from chef_sprout
    
    Change-Id: I45a9085e1f905bcda32e84cf121f983565115fe1

--- device.mk
+++ device.mk
@@ -113,6 +113,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_vendor.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_profiles_vendor.xml
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_SYSTEM)/etc/seccomp_policy/mediacodec.policy
+
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
