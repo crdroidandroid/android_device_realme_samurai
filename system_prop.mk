c597d2cb87af313761640c50e1baaf381dd87734
Harshit Jain <god@harsh1998.me>
2020-07-02 04:05:22 +0530

    RMX1931: sync vendor_props with euclid properties
    * properties that lie under vendor/euclid endup being not set since aosp doesn't look inside of it
    * Selectively pick [Checked by grepping] properties and include them
    
    Change-Id: I7b8163ed2b9602e948ea4fa8254ebeaa1df33621

--- system_prop.mk
+++ system_prop.mk
@@ -6,10 +6,22 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     bt.max.hfpclient.connections=1 \
+    persist.bluetooth.bqr.event_mask=0x8000000E \
+    persist.bluetooth.bqr.min_interval_ms=60000 \
+    persist.bluetooth.bqr.choppy_threshold=9,6 \
+    persist.bluetooth.disableabsvol=false \
+    persist.bluetooth.disableinbandringing=false \
+    persist.bluetooth.specificatcmdsenable=true \
+    persist.vendor.bluetooth.a2dp.hal.implementation=true \
+    persist.vendor.bluetooth.modem_nv_support=true \
+    persist.vendor.bt.a2dp.hal.implementation=true \
     persist.vendor.bt.a2dp.aac_whitelist=false \
     persist.vendor.btstack.enable.splita2dp=true \
     persist.vendor.btstack.enable.twsplus=true \
     persist.vendor.btstack.enable.twsplussho=true \
+    persist.vendor.service.bdroid.soc.alwayson=true \
+    persist.vendor.service.bt.iot.enablelogging=true \
+    persist.vendor.service.bt.iotinfo.report.enable=true \
     ro.bluetooth.a2dp_offload.supported=true \
     ro.bluetooth.library_name=libbluetooth_qti.so \
     qcom.bluetooth.soc=cherokee \
@@ -17,6 +29,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.camera.hfr.enable=1 \
+    ro.camera.relight.enable=0 \
+    ro.camera.attr.detect.enable=1 \
+    ro.camera.temperature.limit=470 \
     persist.vendor.camera.light_ae.enable=1 \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap
 
@@ -38,9 +54,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.disable_backpressure=1 \
     debug.sf.enable_hwc_vds=1 \
+    debug.sf.latch_unsignaled=1 \
     dev.pm.dyn_samplingrate=1 \
-    persist.front.skipdisplay.enable=1
+    persist.front.skipdisplay.enable=1 \
+    ro.vendor.display.sensortype=2 \
+    ro.vendor.display.ad.sdr_calib_data=/vendor/etc/OPPO_OLED_AD_calib.cfg
 
 # IOP
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -90,10 +110,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     DEVICE_PROVISIONED=1 \
     persist.radio.hvolte.enable=1 \
+    persist.radio.custom_exp_ecc=1 \
     persist.sys.fflag.override.settings_network_and_internet_v2=true \
     persist.vendor.radio.atfwd.start=true \
+    persist.vendor.radio.data_con_rprt=1 \
     persist.vendor.radio.lte_vrte_ltd=1 \
     persist.vendor.radio.cs_srv_type=1 \
+    persist.vendor.radio.process_sups_ind=1 \
     persist.vendor.radio.rat_on=combine \
     ril.subscription.types=NV,RUIM \
     ro.telephony.default_network=22,22 \
@@ -108,6 +131,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.ssr.enable_ramdumps=0 \
     persist.vendor.ssr.restart_level=ALL_ENABLE
+
+# IWLAN
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.data.iwlan.enable=true \
+    persist.data.iwlan.rekey=4294967295
 
 # Zygote
 PRODUCT_PROPERTY_OVERRIDES += \
