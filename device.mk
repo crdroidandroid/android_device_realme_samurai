42da93296691231b2ae7ed171b306d158b0718fb
karthick111 <karthickrko61@gmail.com>
2020-10-19 09:18:01 +0530

    RMX1931: Build camera provider impl/service
    Change-Id: I977d9ef36f09d223a7774003971421d09501a053

--- device.mk
+++ device.mk
@@ -126,6 +126,8 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64 \
     Snap
 
 # ContextHub
