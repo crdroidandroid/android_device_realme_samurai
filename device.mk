86a99cbf7b87c1a99d71be17780c4b1ff9068ddf
karthick111 <karthickrko61@gmail.com>
2020-10-20 09:39:29 +0530

    RMX1931: Build default thermal HIDL/HALs
    Change-Id: I1d8be832e0e578344603877b1011790b38a104d0

--- device.mk
+++ device.mk
@@ -367,6 +367,12 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.msmnile
+
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.RMX1931
