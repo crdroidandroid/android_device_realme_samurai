4e3cba9fc92a28ced485ea3814c38565030f1aa6
karthick111 <karthickrko61@gmail.com>
2020-11-23 09:51:41 +0530

    RMX1931: Up rev radio hal versions to match stock
    * IRadio to 1.4
    * IRadioConfig to 1.1
    
    Change-Id: Ic471d059b4090b06dfaa93bd6f1a4d82e2c3e42b

--- device.mk
+++ device.mk
@@ -341,8 +341,8 @@ PRODUCT_PACKAGES += \
 
 # Ril
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.2 \
-    android.hardware.radio.config@1.0 \
+    android.hardware.radio@1.4 \
+    android.hardware.radio.config@1.1 \
     android.hardware.secure_element@1.0 \
     libjson \
     librmnetctl \
