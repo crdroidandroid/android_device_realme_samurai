e935dcef2730bf2f67b1099cc793523ece55566b
karthick111 <karthickrko61@gmail.com>
2020-11-24 08:34:48 +0530

    Revert "RMX1931: Set nfc soong namespace"
    This reverts commit 4a0d798c6567a58eac3b2448a3e7e37d27896001.

--- device.mk
+++ device.mk
@@ -366,8 +366,7 @@ PRODUCT_COPY_FILES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH) \
-    vendor/nxp/opensource/sn100x
+    $(LOCAL_PATH)
 
 # Telephony
 PRODUCT_PACKAGES += \
