0d139d6baee9e40e8ce3c228082cfe977ffc6ced
karthick111 <karthickrko61@gmail.com>
2020-12-28 08:43:13 +0530

    Revert "RMX1931: Include VNDK 29 prebuilts"
    This reverts commit 54a3228dc20e016cd4e9242d42886cdb68f6fdbc.
    
    Change-Id: Iac9bbc8e334f171911586876225a451c1de5c768

--- device.mk
+++ device.mk
@@ -444,9 +444,6 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.2-service.RMX1931
 
 # VNDK
-PRODUCT_TARGET_VNDK_VERSION := 29
-PRODUCT_EXTRA_VNDK_VERSIONS := $(PRODUCT_TARGET_VNDK_VERSION)
-
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libc++.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libc++.so
 
