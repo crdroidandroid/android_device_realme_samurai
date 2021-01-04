bb3fc39affdf0d6ff966f51af66a3617fa07de22
karthick111 <karthickrko61@gmail.com>
2021-01-04 08:39:27 +0530

    RMX1931: Update Media/WFD stack from F.04
    Change-Id: I1062c88bdd5703480b01997992ea61a5212498a5

--- device.mk
+++ device.mk
@@ -370,7 +370,6 @@ PRODUCT_PACKAGES += \
 # QTI
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
-    $(LOCAL_PATH)/configs/product-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
 
 # RenderScript
