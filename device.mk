2f58debf0ab513c304eb706ecf8a9a91d99485b2
karthick111 <karthickrko61@gmail.com>
2021-01-04 20:44:43 +0530

    RMX1931: Build sensor@2.0 multihal
    * Required by updated sensors blobs
    
    Change-Id: Ia7d2f96d6ad4d0dd97bd48c0c3238ec588d31a48

--- device.mk
+++ device.mk
@@ -392,8 +392,8 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl:64 \
-    android.hardware.sensors@1.0-service \
+    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.0-ScopedWakelock.vendor \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
