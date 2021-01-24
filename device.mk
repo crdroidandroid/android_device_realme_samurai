ca88882a82978c75480ccc9010c55daba2d470d4
karthick111 <karthickrko61@gmail.com>
2021-01-24 11:04:22 +0530

    RMX1931: Drop DPM shim
    Change-Id: Ibd548d2778f3e40878558e355a450d22ab03a070

--- device.mk
+++ device.mk
@@ -197,10 +197,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3-service.clearkey
 
-# DPM
-PRODUCT_PACKAGES += \
-    libshim_dpmframework
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.RMX1931 \
