8598605023f590b4fce89f7a0fb2e280e329e054
karthick111 <karthickrko61@gmail.com>
2021-01-27 19:38:33 +0530

    RMX1931: Build & copy 64bit libavservices_minijail
    Change-Id: Iad89eeb9d549fc39b76660ef080606e076183d0b

--- device.mk
+++ device.mk
@@ -329,6 +329,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libavservices_minijail \
     libavservices_minijail_vendor \
+    libavservices_minijail_vendor_64 \
     libc2dcolorconvert \
     libcodec2_hidl@1.0.vendor \
     libcodec2_vndk.vendor \
