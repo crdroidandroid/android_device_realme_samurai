29b70c3ad85296e5d5ae4348aecda3f95d05b102
karthick111 <karthickrko61@gmail.com>
2021-05-19 12:49:45 +0530

    RMX1931: Define TARGET_DEVICE in device makefile
    Change-Id: I6f8167f50b4358f0152d714a9631b4ef36818c9c

--- device.mk
+++ device.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+TARGET_DEVICE := RMX1931
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
