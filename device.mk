a7422a893d827a52e959e913b84b3b51c4cce4a3
ChihYao Chien <ccchien@google.com>
2021-06-07 12:16:52 +0800

    RMX1931: Add qti-telephony-hidl-wrapper-prd
    Provide qti-telephony-hidl-wrapper-prd for modules in product partition to include.
    
    Bug: 189177913
    Bug: 189905530
    Change-Id: I449861fba3d43f983bfb4b99764eae360ccb236c

--- device.mk
+++ device.mk
@@ -449,6 +449,8 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
+    qti-telephony-hidl-wrapper-prd \
+    qti_telephony_hidl_wrapper_prd.xml \
     qti-telephony-utils \
     qti_telephony_utils.xml \
     telephony-ext
