33a6e2ad116b34f46603c8d79655f8f171655334
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-21 02:43:52 +0300

    samurai: sensors: standardize pickup sensor
     * use standard sensor type
     * ignore non-wakeup sensor variant
     * ignore events that do not properly match a pickup
    
     * currently inverted (bug)
    
    Change-Id: Iae51ff005290769d3bf905f2f249c18c900d1e6b
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- sensors/HalProxy.cpp
+++ sensors/HalProxy.cpp
@@ -83,6 +83,25 @@ int64_t msFromNs(int64_t nanos) {
     return nanos / nanosecondsInAMillsecond;
 }
 
+bool patchOPPickupSensor(V2_1::SensorInfo& sensor) {
+    if (sensor.typeAsString != "android.sensor.tilt_detector") {
+        return true;
+    }
+
+    /*
+     * Implement only the wake-up version of this sensor.
+     */
+    if (!(sensor.flags & V1_0::SensorFlagBits::WAKE_UP)) {
+        return false;
+    }
+
+    sensor.type = V2_1::SensorType::PICK_UP_GESTURE;
+    sensor.typeAsString = SENSOR_STRING_TYPE_PICK_UP_GESTURE;
+    sensor.maxRange = 1;
+
+    return true;
+}
+
 HalProxy::HalProxy() {
     const char* kMultiHalConfigFile = "/vendor/etc/sensors/hals.conf";
     initializeSubHalListFromConfigFile(kMultiHalConfigFile);
@@ -499,6 +518,10 @@ void HalProxy::initializeSensorList() {
                         sensor.type = SensorType::LIGHT;
                         ALOGV("Replaced Wise Light sensor with standard light sensor");
                         AlsCorrection::init();
+                    }
+                    bool keep = patchOPPickupSensor(sensor);
+                    if (!keep) {
+                        continue;
                     }
 
                     mSensors[sensor.sensorHandle] = sensor;
