b1f1a1021a7c9201305a8d3f501ab8e40c6d7a5f
karthick111 <karthickrko61@gmail.com>
2022-01-02 10:14:54 +0530

    RMX1931: Update fingerprint lockstate prop
    Change-Id: I1e046316c63e9cd4d958706b947b8dd4b0b6684c

--- extract-files.sh
+++ extract-files.sh
@@ -51,6 +51,9 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+        odm/lib64/libgf_hal_G3.so)
+            sed -i "s/ro.boot.flash.locked/vendor.flash.locked\x00/" "${2}"
+            ;;
         system_ext/etc/permissions/qcrilhook.xml)
             ;&
         system_ext/etc/permissions/telephonyservice.xml)
