04a6dd61e57558a9558fa3ac8bd48a92367cbc2d
karthick111 <karthickrko61@gmail.com>
2022-01-30 19:21:45 +0530

    RMX1931: recovery: Include libbase in shared libraries
    Change-Id: Idd3adb9b785b16f64c0087681cd89e265a52c748

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -5,12 +5,12 @@ include $(CLEAR_VARS)
 LOCAL_C_INCLUDES := bootable/recovery \
                     bootable/recovery/edify/include \
                     bootable/recovery/otautil/include \
-                    bootable/recovery/updater/include \
-                    system/core/base/include
+                    bootable/recovery/updater/include
 ifneq ($(TARGET_PLATFORM_DEVICE_BASE),)
 LOCAL_CFLAGS += -DUSES_BOOTDEVICE_PATH
 endif
 
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_qcom
+LOCAL_SHARED_LIBRARIES := libbase
 include $(BUILD_STATIC_LIBRARY)
