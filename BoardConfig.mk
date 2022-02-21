1fe8494939d33c14914f6683e0fcc5970001fd8f
karthick111 <karthickrko61@gmail.com>
2022-02-21 08:38:47 +0530

    RMX1931: Bump Vendor SPL
    Change-Id: Iaac5915215730271d5cfac2bcf050bb6bf9147f6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -139,7 +139,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-07-05
+VENDOR_SECURITY_PATCH := 2021-11-05
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
