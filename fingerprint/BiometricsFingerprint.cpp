2d73faa25055ae9c19bd2bcb680880b9eb826167
karthick111 <karthickrko61@gmail.com>
2022-03-26 10:03:57 +0530

    RMX1931: fingerprint: Return FingerprintAcquiredInfo on onTouch(Up/Down)
    Change-Id: I3bc0af863769defcd43dde67d0a4f2f15d7f8a4e

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -101,8 +101,13 @@ public:
         return Void();
     }
 
-    Return<void> onTouchUp(uint64_t deviceId) { return Void(); }
-    Return<void> onTouchDown(uint64_t deviceId) { return Void(); }
+    Return<void> onTouchUp(uint64_t deviceId) {
+        return mClientCallback->onAcquired(deviceId, android::hardware::biometrics::fingerprint::V2_1::FingerprintAcquiredInfo::ACQUIRED_VENDOR, 0);
+	}
+
+    Return<void> onTouchDown(uint64_t deviceId) {
+        return mClientCallback->onAcquired(deviceId, android::hardware::biometrics::fingerprint::V2_1::FingerprintAcquiredInfo::ACQUIRED_VENDOR, 1);
+	}
     Return<void> onSyncTemplates(uint64_t deviceId, const hidl_vec<uint32_t>& fingerId, uint32_t remaining) {
         ALOGE("onSyncTemplates %lu %zu %u", deviceId, fingerId.size(), remaining);
         myDeviceId = deviceId;
