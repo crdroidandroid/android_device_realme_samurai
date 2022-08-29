efaff45789a934dbd627857f4ee07e11a74e8fb4
Edwin Moquete <edwinmmoquete@gmail.com>
2022-08-29 01:37:22 +0000

    RMX1931: Switch to legacy sepolicy_vndr
    Change-Id: I13229c3057bf32c81b0fdace075f0791cb8ad380

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -138,7 +138,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2021-11-05
 
 # Sepolicy
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
