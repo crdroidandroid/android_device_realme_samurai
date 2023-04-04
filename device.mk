8070428129b8be1147ec6cdbb7ceb7fb5056cd29
onettboots <blackcocopet@gmail.com>
2023-04-04 22:24:57 +0000

    RMX1931: Drop mock power stats
     E/android.hardware.power.stats@1.0-service-mock(787): Failed to getEnergyData
    
    Change-Id: I38ca9c8a2f868c4e260428b75974b9f05459139b

--- device.mk
+++ device.mk
@@ -383,7 +383,6 @@ $(call inherit-product, packages/apps/RealmeParts/parts.mk)
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     android.hardware.power@1.2.vendor \
-    android.hardware.power.stats@1.0-service.mock \
     vendor.qti.hardware.perf@2.2.vendor
 
 # Preopt SystemUI
