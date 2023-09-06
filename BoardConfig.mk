6d34519891da18d45363e60cb4a91b39cce55b65
karthick111 <karthickrko61@gmail.com>
2023-09-06 19:07:36 +0200

    samurai: Move syshelper sepolicies to sepolicy dir
    Change-Id: I6f8d9e045f3edc45ff6fe21f2fd6739a0bd07925

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -143,7 +143,6 @@ include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/syshelper/sepolicy
 
 # Power
 TARGET_USES_INTERACTION_BOOST := true
