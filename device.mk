1ff19588dac72f7399de0e44fba73731f9b02e51
karthick111 <karthickrko61@gmail.com>
2023-12-12 16:48:25 +0530

    samurai: Update QC common blobs
    From: LA.UM.9.1.r1-13700-SMxxx0.QSSI13.0
    
    Change-Id: I71b80daec5be9320d110d56541ab4900d0ac1de9

--- device.mk
+++ device.mk
@@ -437,6 +437,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
+# Service Tracker
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.servicetracker@1.2.vendor
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
